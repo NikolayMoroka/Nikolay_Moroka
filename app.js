933d0166ced0c34ee81a5ad1352c652f4ea506d0
NikolayMoroka <nikolaymoroka@gmail.com>
2020-11-28 17:59:41 +0200

    renewal

--- app.js
+++ app.js
@@ -18,14 +18,14 @@ function createH(tagName, clName, text) {
     }, 1);
     setTimeout(() => {
         createH('h1','title2', 'FRONT-END'); 
-    }, 500);
+    }, 1000);
     setTimeout(() => {
         createH('h1','title3', 'DEVELOPER'); 
-    }, 1000);
+    }, 1500);
     setTimeout(() => {
         createH('p', 'animation-type-backInUp', 'My name is Nikolay Moroka');
         createH('p', 'animation-type-backInUp', 'I will create the best website for your buisness');
-    }, 2000);
+    }, 2500);
    
 })();
 
