2a8ba3057ecf46e4577ee35a90c97bc4f7f75be5
NikolayMoroka <nikolaymoroka@gmail.com>
2020-12-04 18:08:19 +0200

    test

---  sandbox/app-testing.js
+++  sandbox/app-testing.js
@@ -1,8 +1,8 @@
 const first = document.getElementById('first');
 
-function createInfoSkills(tagName, clName, id, text) {
+function createInfoSkills(tagName, id, text) {
     let secondP = document.createElement(tagName);
-    secondP.classList.add(clName, 'style_text');
+    secondP.classList.add('style_text');
     secondP.id = (id);
     secondP.innerHTML = text;
     first.append(secondP);
@@ -15,49 +15,93 @@ function scroller() {
         if(flag == 0) {
             if(window.pageYOffset > 10) {
                 setTimeout(() => {
-                    const html = document.getElementById('html');
-                        let secondP = document.createElement('p');
-                        secondP.classList.add('style_text');
-                        secondP.innerHTML = 'HTML';
-                        html.append(secondP);
+                    createInfoSkills('p', 'html', 'HTML')
                 }, 1);
                 setTimeout(() => {
-                    const css = document.getElementById('css');
-                    let secondP = document.createElement('p');
-                        secondP.classList.add('style_text');
-                        secondP.innerHTML = 'CSS';
-                        css.append(secondP);
-                }, 1);
+                    createInfoSkills('p', 'css', 'CSS')
+                }, 200);
                 setTimeout(() => {
-                    const js = document.getElementById('js');
-                    let secondP = document.createElement('p');
-                        secondP.classList.add('style_text');
-                        secondP.innerHTML = 'JavaScript';
-                        js.append(secondP);
-                }, 1);
-               
-
-                     
+                    createInfoSkills('p', 'js', 'JavaScript')
+                }, 400);    
             flag = 1;
         }
        
     }
 }
 
-const img = document.getElementById('img')
 
-const htmlTg = document.getElementById('html');
-htmlTg.addEventListener('mouseover', e => {
+
+document.getElementById('first').addEventListener('mouseover', function(e){
+    if(e.target.id == 'first') {
+        return
+    } else if(e.target.id == 'html') {
+        createImg('img', 'src', '../img/html.png')
+        let id = e.target.id;
+        console.log(id);
+    } else if(e.target.id == 'css') {
+        createImg('img', 'src', '../img/css.png')
+    } else if(e.target.id == 'js') {
+        createImg('img', 'src', '../img/js.png')
+        createImg('img', 'src', '../img/jquery.png')
+        createImg('img', 'src', '../img/vue.png')
+    } else {
+        return
+    }
+    });
+
     
-    let firstImg = document.createElement('img');
-    firstImg.classList.add('animation_fadeInRight', 'img');
-    firstImg.setAttribute('src', '../img/html.png')
-    img.append(firstImg);
-})
 
-const imgDel = document.getElementsByClassName('img');
-imgDel.addEventListener('mouseout', e => {
-    imgDel.remove();
-})
+// document.getElementById('first').addEventListener('mouseout', function(e){
+//     if(e.target.id == 'first' && 'html') {
+//         return
+//     } else {
+     
+//      let idS = img.id;
+//      console.log(idS);
+//      document.querySelector().remove();
+//    };
+//    if(e.target.id == 'first' && 'html') {
+//     return
+//     } else {
+
+//     document.getElementById('imgHtml').remove();
+//     };
+//     if(e.target.id == 'first' && 'html') {
+//         return
+//     } else {
+
+//     document.getElementById('imgHtml').remove();
+//     };
+// });
+
+// document.getElementById('first').addEventListener('mouseout', function(e){ 
+//     if(e.target.id == 'first' && 'html') {
+//         return
+//     } else {
+//     document.querySelectorAll('#img');
+//     let id = e.target.id;
+//     console.log(id);
+//     // document.getElementById(id).remove();
+//     } 
+// });
+
+
+
+
+function createImg(tagName, atrib, value) {
+    const img = document.getElementById('img')
+    let firstImg = document.createElement(tagName);
+    firstImg.id = genIDge();
+    firstImg.setAttribute(atrib, value)
+    img.append(firstImg);
+}
+
+function genIDge(value) {
+    var firstPart = (Math.random() * 46656) | 0;
+    var secondPart = (Math.random() * 46656) | 0;
+    firstPart = ("000" + firstPart.toString(36)).slice(-3);
+    secondPart = ("000" + secondPart.toString(36)).slice(-3);
+    return firstPart + secondPart;
+}
 
 
